d1d0a9eecf7c91956eb8f1beafd6b8b370e4c9ce
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-08-10 19:18:36 -0500

    Consecutive Numbers, MySQL, MSSQL, LeetCode 180

--- consecutive-numbers.sql
+++ consecutive-numbers.sql
@@ -1,4 +1,11 @@
 -- MySQL, MSSQL
+-- Using Joins
+SELECT DISTINCT a.num AS ConsecutiveNums
+FROM Logs a
+         JOIN Logs b ON a.id = b.id + 1 AND a.num = b.num
+         JOIN Logs c ON a.id = c.id + 2 AND a.num = c.num
+
+-- Using With & Window Functions
 -- WITH cte AS
 -- (SELECT *,
 --  LEAD(num, 1) OVER() AS next_value_1,

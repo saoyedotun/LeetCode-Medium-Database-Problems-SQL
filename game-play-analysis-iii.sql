78d36f34e92ea000d1b857836b3fcddeef82a7fb
saoyedotun <oyedotunsodiq045@yahoo.com>
2022-08-11 10:25:05 -0500

    Game Play Analysis III, MySQL, PL/SQL, LeetCode 534

--- game-play-analysis-iii.sql
+++ game-play-analysis-iii.sql
@@ -1,4 +1,4 @@
--- MySQL, Oracle PL/SQL, MSSQL
+-- MySQL, Oracle PL/SQL
 SELECT a1.player_id, a1.event_date, SUM(a2.games_played) AS games_played_so_far
 FROM Activity a1, Activity a2
 WHERE a1.player_id = a2.player_id
